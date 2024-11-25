1a88fe0ae90bce42d966fb91fc110d228416a22d
Eduardo Kraus <kraus@eduardokraus.com>
2024-11-24 22:21:04 -0300

    dependencies

--- version.php
+++ version.php
@@ -28,3 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2024011901;      // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022111800;      // Requires this Moodle version.
 $plugin->component = "media_cloudstudio"; // Full name of the plugin (used for diagnostics).
+
+$plugin->dependencies = [
+    "mod_cloudstudio" => 2024102401,
+];
